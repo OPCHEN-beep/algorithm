4557cfb20bc9e64a7fe99ff3bce73f3335c53446
OPCHEN-beep <462416217@qq.com>
2026-01-24 20:01:03 +0800

    1

---
+++ C++/未命名1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    char a;
+    cin >> a;
+    cout << " " << " " << a;
+    cout << " " << a << a << a;
+    cout << a << a << a << a << a;
+
+    return 0;
+}
